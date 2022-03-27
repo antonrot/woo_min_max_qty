63a8ec9b4d8923988921826a567524c863d0135a
Anton <antonr@elementor.com>
2022-03-27 17:43:11 +0300

    added min max fields for the variation products

--- settings/woo_min_max_qty_settings.php
+++ settings/woo_min_max_qty_settings.php
@@ -9,8 +9,52 @@ if ( ! class_exists('Woo_min_max_qty_settings' ) ) {
 		public function __construct() {
 			add_filter( 'woocommerce_quantity_input_args',  [ $this, 'quantity_stock_base_args' ], 10, 2 );
 			add_action( 'woocommerce_product_options_inventory_product_data', [ $this, 'add_min_max_qty_fields' ] );
-			add_action( 'woocommerce_process_product_meta', [ $this, 'save_product_min_max_qty_fields' ] );
+            add_action( 'woocommerce_process_product_meta', [ $this, 'save_product_min_max_qty_fields' ] );
+            add_action( 'woocommerce_variation_options_dimensions', [ $this, 'add_min_max_qty_fields_variation_product' ], 10 ,3 );
+            add_action( 'woocommerce_save_product_variation', [ $this, 'save_product_min_max_qty_variation_product_fields' ], 10, 2 );
 		}
+
+        public function save_product_min_max_qty_variation_product_fields( $variation_id, $index ) {
+
+            $meta_keys = [
+                'min_qty',
+                'max_qty',
+            ];
+
+            foreach ( $meta_keys as $meta_key ) {
+                if ( isset( $_POST[ $meta_key ][ $index ] ) ) {
+                    update_post_meta( $variation_id, $meta_key, sanitize_text_field( $_POST[ $meta_key ][ $index ] ) );
+                }
+            }
+        }
+
+        public function add_min_max_qty_fields_variation_product($i, $variation_data, $variation) {
+
+            woocommerce_wp_text_input(
+                [
+                    'id' => 'min_qty['. $i .']',
+                    'label' => 'Minimum quantity',
+                    'wrapper_class' => 'form-row',
+                    'desc_tip' => true,
+                    'description' => 'Specify minimum allowed product quantity for orders to be completed.',
+                    'value' => get_post_meta( $variation->ID, 'min_qty', true ),
+                    'type' => 'number'
+                ]
+            );
+
+            woocommerce_wp_text_input(
+                [
+                    'id' => 'max_qty['. $i .']',
+                    'label' => 'Maximum quantity',
+                    'wrapper_class' => 'form-row',
+                    'desc_tip' => true,
+                    'description' => 'Specify maximum allowed product quantity for orders to be completed.',
+                    'value' => get_post_meta( $variation->ID, 'max_qty', true ),
+                    'type' => 'number'
+                ]
+            );
+
+        }
 
 		public function add_min_max_qty_fields(){
 			echo '<div class="option_group">';
@@ -41,8 +85,8 @@ if ( ! class_exists('Woo_min_max_qty_settings' ) ) {
 		}
 
 		public function save_product_min_max_qty_fields( $post_id ) {
-			update_post_meta( $post_id, 'min_qty', intval( $_POST[ 'min_qty' ] ) );
-			update_post_meta( $post_id, 'max_qty', intval( $_POST[ 'max_qty' ] ) );
+			update_post_meta( $post_id, 'min_qty', sanitize_text_field( $_POST[ 'min_qty' ] ) );
+			update_post_meta( $post_id, 'max_qty', sanitize_text_field( $_POST[ 'max_qty' ] ) );
 		}
 
 		public function quantity_stock_base_args($args, $product) {
@@ -56,7 +100,7 @@ if ( ! class_exists('Woo_min_max_qty_settings' ) ) {
 
 			if( $product->get_manage_stock() ) {
 
-				if( $product_in_stock < 10 ) {
+				if( $product_in_stock < $max ) {
 					$args[ 'max_value' ] = $product_in_stock;
 				}
 
